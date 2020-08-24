9fda8dbbc4ea97d5d2d978e852e9c73d398e2687
gaev-art <forsakensr@gmail.com>
2020-08-24 10:20:22 +0300

    Updates

---
+++ precache-manifest.603605d642b5d696952c688210e6a6fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d78029f52d6438b4043b741c0c31c8",
+    "url": "/My-Social-Network/index.html"
+  },
+  {
+    "revision": "c6cc6f79f4bc1f79aed6",
+    "url": "/My-Social-Network/static/css/main.c2ccb857.chunk.css"
+  },
+  {
+    "revision": "ee4a151e44909b4648d8",
+    "url": "/My-Social-Network/static/js/2.d7128583.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-Social-Network/static/js/2.d7128583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6cc6f79f4bc1f79aed6",
+    "url": "/My-Social-Network/static/js/main.97bb1ffb.chunk.js"
+  },
+  {
+    "revision": "eac8888b77fa88f4ab09",
+    "url": "/My-Social-Network/static/js/runtime-main.e7c54d80.js"
+  }
+]);
